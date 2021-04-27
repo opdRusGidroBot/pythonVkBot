7c088bc2d475a95ed571f0709b947a9a01336d28
Valosis <avto_largus@mail.ru>
2021-04-27 21:12:56 +0300

    New bot base structure

--- bot.py
+++ bot.py
@@ -11,8 +11,6 @@ vk = vk_session.get_api()
 from vk_api.longpoll import VkLongPoll, VkEventType#импортируем модуль Long pool для личных сообщений
 Lslongpoll = VkLongPoll(vk_session)
 Lsvk = vk_session.get_api()
-keyboard = VkKeyboard(one_time=True)
-keyboard.add_button('Викторина', color=VkKeyboardColor.NEGATIVE)
 
 
 class student():
@@ -25,6 +23,7 @@ class student():
         self.free_quest = numpy.zeros(size,dtype=int)
         self.score = 0
         self.already = 0
+        self.game = 0
 
 
 def count_lines(filename):
@@ -63,7 +62,6 @@ def get_quest(filename,cur,quest,vars):
             if(flag==size):
                 cur.free_quest.fill(0)
     cur.free_quest[number]=1
-    cur.already+=1
     number+=1
     for i in range(number*2):
         line = a.readline()
@@ -72,12 +70,15 @@ def get_quest(filename,cur,quest,vars):
     a.close()
     vars = vars.strip()
     vars = vars.split(';')
+    cur.already+=1
     return quest,vars
 
 
 def print_start(Nmessage):
     keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button('Start', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_button('Старт', color=VkKeyboardColor.POSITIVE )
+    keyboard.add_line()
+    keyboard.add_button('Назад', color=VkKeyboardColor.SECONDARY )
     Lsvk.messages.send(
                        user_id = event.user_id,
                        random_id = get_random_id(),
@@ -94,7 +95,7 @@ def print_variants(quest,vars):
     else :
         keyboard.add_button("Я не знаю(", color=VkKeyboardColor.NEGATIVE)
     keyboard.add_line()
-    keyboard.add_button("Закончить викторину", color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_button("Закончить викторину", color=VkKeyboardColor.SECONDARY)
     Lsvk.messages.send(
                        user_id = event.user_id,
                        random_id = get_random_id(),
@@ -104,14 +105,12 @@ def print_variants(quest,vars):
 
 
 def exit(where,Nmessage,cur,lock):
-    keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button('Start', color=VkKeyboardColor.NEGATIVE)
-    Lsvk.messages.send(
-                       user_id = event.user_id,
-                       random_id = get_random_id(),
-                       keyboard = keyboard.get_keyboard(),
-                       message = Nmessage
-                       )
+    if Nmessage:
+        Lsvk.messages.send(
+                           user_id = event.user_id,
+                           random_id = get_random_id(),
+                           message = Nmessage
+                           )
     cur.position = where
     return lock+1
 
@@ -121,20 +120,18 @@ def check_answers(event,quest,cur):
         Lsvk.messages.send(
                            user_id = event.user_id,
                            random_id = get_random_id(),
-                           keyboard = keyboard.get_keyboard(),
-                           message = 'Ваш ответ верный!'
+                           message = 'Ваш ответ верный!',
                            )
     else:
         Lsvk.messages.send(
                            user_id = event.user_id,
                            random_id = get_random_id(),
-                           keyboard = keyboard.get_keyboard(),
-                           message = 'Ваш ответ не верный('
+                           message = 'Ваш ответ не верный(',
                            )
 def out_result(cur):
     keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button("Еще раз", color=VkKeyboardColor.NEGATIVE)
-    keyboard.add_button("Выход", color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_button("Еще раз", color=VkKeyboardColor.POSITIVE )
+    keyboard.add_button("Выход", color=VkKeyboardColor.SECONDARY )
     output = 'Викторина окончена и вы набрали '+str(cur.score)+' очков'
     Lsvk.messages.send(
                    user_id = event.user_id,
@@ -142,11 +139,62 @@ def out_result(cur):
                    keyboard = keyboard.get_keyboard(),
                    message = output
                    )
-    cur.score=0
-    cur.already=0
+    cur.score = 0
+    cur.already = 0
+    cur.game = 0
 
 
-filename = 'quests.txt'
+def out_lobby(msg):
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Расписание', color=VkKeyboardColor.SECONDARY )
+    keyboard.add_line()
+
+    keyboard.add_button('Обратная связь', color=VkKeyboardColor.PRIMARY )
+    keyboard.add_line()
+
+    keyboard.add_button('Информация', color=VkKeyboardColor.POSITIVE )
+    keyboard.add_line()
+
+    keyboard.add_button('Викторина', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+
+    keyboard.add_button('Новости', color=VkKeyboardColor.NEGATIVE)
+
+    Lsvk.messages.send(
+                       user_id = event.user_id,
+                       random_id = get_random_id(),
+                       keyboard = keyboard.get_keyboard(),
+                       message = msg,
+                       )
+                       
+                      
+def out_info(keyboard):
+    keyboard.add_button('Общая информация', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Информация о ВУЗах-партнерах', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_line()
+    keyboard.add_button('Олимпиады', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Назад', color=VkKeyboardColor.PRIMARY)
+                           
+def get_position(msg):
+    if msg == "Викторина":
+        return "quiz"
+    
+    elif msg == "Расписание":
+        return "schedule"
+    
+    elif msg == "Информация":
+        return "info"
+    
+    elif msg == "Обратная связь":
+        return "feedback"
+        
+    elif msg == "Новости":
+        return "news"
+
+
+filename = 'questionsForQuiz.txt'
 
 ListOfStudents=[]
 quest=[]
@@ -156,7 +204,6 @@ size = int((count_lines(filename)-1) / 2)
 maxsize = get_maxsize(filename)
 lock = 0
 
-
 for event in Lslongpoll.listen():#слушаем longpool на предмет новых сообщений. event — переменная в которой будет храниться само сообщение и некоторые данные о нем.
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text and event.from_user:
         cur = check(ListOfStudents,event.user_id,size)
@@ -164,49 +211,104 @@ for event in Lslongpoll.listen():#слушаем longpool на предмет н
         while lock:
             if cur.position == "lobby":
                 lock -= 1
-                vars1 = ['Привет', 'Ку', 'Хай', 'Хеллоу']
-                if event.text in vars1:
-                    keyboard = VkKeyboard(one_time=True)
-                    keyboard.add_button('Викторина', color=VkKeyboardColor.NEGATIVE)
-                    Lsvk.messages.send(
-                                       user_id = event.user_id,
-                                       random_id = get_random_id(),
-                                       keyboard = keyboard.get_keyboard(),
-                                       message = 'Привет, выбери снизу интересующую кнопку!)',
-                                       )
-                elif event.text == "Викторина":
-                    cur.position = "victorina"
+                greetings = ['Привет', 'Ку', 'Хай', 'Хеллоу']
+                listOfOptions = ['Викторина','Расписание','Обратная связь','Информация', 'Новости']
+                if event.text in greetings:
+                    out_lobby('Привет, выбери снизу интересующую кнопку!)')
+                elif event.text in listOfOptions:
+                    cur.position = get_position(event.text)
                     lock += 1
                 else:
-                    keyboard = VkKeyboard(one_time=True)
-                    keyboard.add_button('Викторина', color=VkKeyboardColor.NEGATIVE)
+                    out_lobby('Выбери снизу интересующую кнопку!')
+            elif cur.position == "info":
+                lock-=1
+                university = ['Информация о ВУЗах-партнерах']
+                olimpyc = ['Олимпиады']
+                exitWordsForInfo = ['Назад']
+                genral_info = ['Общая информация']
+                keyboard = VkKeyboard(one_time=True)
+                out_info(keyboard)
+                if event.text in university:
                     Lsvk.messages.send(
                                        user_id = event.user_id,
                                        random_id = get_random_id(),
                                        keyboard = keyboard.get_keyboard(),
-                                       message = 'You are in lobby',
+                                       message = 'Благодаря Энергоклассам ПАО "РусГидро" у тебя есть шанс поступить в наши ВУЗы-партнеры, среди которых: Санкт-Петербургский политехнический университети им.Петра Великого в Институт...',
                                        )
-            if cur.position == "victorina":
+                elif event.text in olimpyc:
+                   Lsvk.messages.send(
+                                       user_id = event.user_id,
+                                       random_id = get_random_id(),
+                                       keyboard = keyboard.get_keyboard(),
+                                       message = 'У нас ты сможешь принять участие в олимпиадах от ПАО "РусГидро", такие как: Отраслевая олимпиада школьников «Энергия образования» и другие Всероссийские олимпиады! При успешном участии...',
+                                       )
+                elif event.text in genral_info:
+                    Lsvk.messages.send(
+                                       user_id = event.user_id,
+                                       random_id = get_random_id(),
+                                       keyboard = keyboard.get_keyboard(),
+                                       message = 'РусГидро приглашает школьников девяти регионов России в Энергоклассы и на факультативные занятия по теории решения изобретательских задач (ТРИЗ). В рамках образовательного проекта Энергоклассов учащиеся 9-11 классов...',
+                                       )
+                elif event.text in exitWordsForInfo:
+                    lock = exit('lobby','',cur,lock)
+                else :
+                    Lsvk.messages.send(
+                                       user_id = event.user_id,
+                                       random_id = get_random_id(),
+                                       keyboard = keyboard.get_keyboard(),
+                                       message = 'Выбери снизу интересующую кнопку!',
+                                       )
+            
+                   
+            elif cur.position =="schedule":
+                lock-=1
+                # тело расписания
+                #if event.text in exitWordsForSchedule: 
+                #    lock = exit('lobby','',cur, lock) Выход из Расписания закачивать этой функцией 
+                
+
+            elif cur.position =="feedback":
+                lock-=1
+                # тело обратной связи
+                #if event.text in exitWordsForFeedback:
+                #   lock = exit('lobby','',cur, lock) Выход из Обратной связи закачивать этой функцией 
+                
+
+            elif cur.position =="news":
+                lock-=1
+                # тело обратной связи
+                #if event.text in exitWordsForNews:
+                #   lock = exit('lobby','',cur, lock) Выход из Новостей закачивать этой функцией 
+
+                
+            elif cur.position == "quiz":
                 lock -= 1
-                hello = ['Викторина']
-                start = ['Start','Еще раз']
-                end = ['Закончить викторину','Выход']
+                greetingWordsForQuiz = ['Викторина']
+                startWordsForQuiz = ['Старт','Еще раз']
+                exitWordsForQuiz = ['Закончить викторину','Выход','Назад']
 
-                if event.text in hello:
-                    print_start('Для начала викторины жми \"Start\"')
+                if event.text in greetingWordsForQuiz:
+                    print_start('Для начала викторины жми \"Старт\"')
 
-                elif event.text in start:
+                elif event.text in startWordsForQuiz:
+                    cur.game = 1
                     quest,vars = get_quest(filename,cur,quest,vars)
                     print_variants(quest,vars)
-
-                elif event.text in end: #Выход
+                
+                elif event.text in exitWordsForQuiz: #Выход
+                    cur.game = 0
+                    cur.already = 0
+                    cur.score = 0
                     lock = exit('lobby','Удачи!',cur, lock)
+                elif cur.game == 1:
+                    if cur.already<maxsize:
+                        check_answers(event,quest,cur)
+                        quest,vars = get_quest(filename,cur,quest,vars)
+                        print_variants(quest,vars)
 
-                elif cur.already<maxsize:
-                    check_answers(event,quest,cur)
-                    quest,vars = get_quest(filename,cur,quest,vars)
-                    print_variants(quest,vars)
-
-                else:
-                    check_answers(event,quest,cur)
-                    out_result(cur)
+                    else:
+                        check_answers(event,quest,cur)
+                        out_result(cur)
+                    
+                else :
+                    print_start('Попробуй ввести что-то другое')
