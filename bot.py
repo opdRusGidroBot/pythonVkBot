bc085ea109b8ab6ad71b94d7ce656c515e976667
IlyaVeryCool <eremenko.ilya03@mail.ru>
2021-04-27 22:27:46 +0300

    job with updated code

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
-
+import datetime
 import vk_api  # импортируем библиотеки
 import vk
+import random
+import numpy
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor  # импортируем нужные модули
 from vk_api.utils import get_random_id
-import datetime
 
 vk_session = vk_api.VkApi(
     token='6181ac09755b06b499b0aed67cb6ff0b3cbbb7d8cc598a0f5c311b31fa5e252eff8dfb48c3170b8a1c34f')  # авторизируемся
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType  # импортируем нужные модули
-from vk_api.bot_longpoll import VkBotLongPoll  # импортируем нужные модули
 
 longpoll = VkBotLongPoll(vk_session, 203652377)
 vk = vk_session.get_api()
@@ -18,9 +18,173 @@ from vk_api.longpoll import VkLongPoll, VkEventType  # импортируем м
 
 Lslongpoll = VkLongPoll(vk_session)
 Lsvk = vk_session.get_api()
-keyboard = VkKeyboard(one_time=True)
-keyboard.add_button('Информация', color=VkKeyboardColor.NEGATIVE)
-keyboard.add_button('Обратная связь', color=VkKeyboardColor.NEGATIVE)
+
+
+class student():
+    """Модель студента"""
+
+    def __init__(self, nid, position, size):
+        self.id = nid
+        self.position = position
+        self.free_quest = numpy.zeros(size, dtype=int)
+        self.score = 0
+        self.already = 0
+        self.game = 0
+
+
+def count_lines(filename):
+    return sum(1 for line in open(filename, 'r'))
+
+
+def get_maxsize(filename):
+    a = open(filename, 'r')
+    line = a.readline()
+    line = line.strip()
+    a.close()
+    return int(line)
+
+
+def check(list, nid, size):
+    current = 0
+    for i in list:
+        if i.id == nid:
+            current = i
+    if current:
+        return current
+    else:
+        list.append(student(nid, "lobby", size))
+        return list[-1]
+
+
+def get_quest(filename, cur, quest, vars):
+    a = open(filename, 'r')
+    number = random.randint(1, size) - 1
+    if (cur.free_quest[number] == 1):
+        flag = 0
+        while cur.free_quest[number]:
+            number += 1
+            number %= size
+            flag += 1
+            if (flag == size):
+                cur.free_quest.fill(0)
+    cur.free_quest[number] = 1
+    number += 1
+    for i in range(number * 2):
+        line = a.readline()
+    quest = line.split('"')
+    vars = a.readline()
+    a.close()
+    vars = vars.strip()
+    vars = vars.split(';')
+    cur.already += 1
+    return quest, vars
+
+
+def print_start(Nmessage):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Старт', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_line()
+    keyboard.add_button('Назад', color=VkKeyboardColor.SECONDARY)
+    Lsvk.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=Nmessage,
+    )
+
+
+def print_variants(quest, vars):
+    keyboard = VkKeyboard(one_time=True)
+    if (len(vars) > 1):
+        for letter in vars:
+            keyboard.add_button(str(letter), color=VkKeyboardColor.NEGATIVE)
+    else:
+        keyboard.add_button("Я не знаю(", color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button("Закончить викторину", color=VkKeyboardColor.SECONDARY)
+    Lsvk.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=str(quest[1]),
+    )
+
+
+def exit(where, Nmessage, cur, lock):
+    if Nmessage:
+        Lsvk.messages.send(
+            user_id=event.user_id,
+            random_id=get_random_id(),
+            message=Nmessage
+        )
+    cur.position = where
+    return lock + 1
+
+
+def check_answers(event, quest, cur):
+    if len(event.text) == len(quest[3]) and event.text in quest[3]:
+        cur.score += 1
+        Lsvk.messages.send(
+            user_id=event.user_id,
+            random_id=get_random_id(),
+            message='Ваш ответ верный!',
+        )
+    else:
+        Lsvk.messages.send(
+            user_id=event.user_id,
+            random_id=get_random_id(),
+            message='Ваш ответ не верный(',
+        )
+
+
+def out_result(cur):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button("Еще раз", color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button("Выход", color=VkKeyboardColor.SECONDARY)
+    output = 'Викторина окончена и вы набрали ' + str(cur.score) + ' очков'
+    Lsvk.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=output
+    )
+    cur.score = 0
+    cur.already = 0
+    cur.game = 0
+
+
+def out_lobby(msg):
+    keyboard = VkKeyboard(one_time=True)
+
+    keyboard.add_button('Расписание', color=VkKeyboardColor.SECONDARY)
+    keyboard.add_line()
+
+    keyboard.add_button('Обратная связь', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_line()
+
+    keyboard.add_button('Информация', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_line()
+
+    keyboard.add_button('Викторина', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+
+    keyboard.add_button('Новости', color=VkKeyboardColor.NEGATIVE)
+
+    Lsvk.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=keyboard.get_keyboard(),
+        message=msg,
+    )
+
+
+def out_info(keyboard):
+    keyboard.add_button('Общая информация', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Информация о ВУЗах-партнерах', color=VkKeyboardColor.POSITIVE)
+    keyboard.add_line()
+    keyboard.add_button('Олимпиады', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Назад', color=VkKeyboardColor.PRIMARY)
 
 def  feedback(event):
     if not os.path.isdir("Feedback"):  # проверяем есть ли папка, если нет, то создаем
@@ -87,90 +251,142 @@ def  feedback(event):
             break
 
 
+def get_position(msg):
+    if msg == "Викторина":
+        return "quiz"
+
+    elif msg == "Расписание":
+        return "schedule"
+
+    elif msg == "Информация":
+        return "info"
+
+    elif msg == "Обратная связь":
+        return "feedback"
+
+    elif msg == "Новости":
+        return "news"
+
+
+filename = 'questionsForQuiz.txt'
+
+ListOfStudents = []
+quest = []
+vars = []
+
+size = int((count_lines(filename) - 1) / 2)
+maxsize = get_maxsize(filename)
+lock = 0
 
 for event in Lslongpoll.listen():  # слушаем longpool на предмет новых сообщений. event — переменная в которой будет храниться само сообщение и некоторые данные о нем.
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        vars0 = ['Обратная связь']
-        if event.text in vars0:
-            feedback(event,Lslongpoll,Lsvk)
-        vars1 = ['Привет', 'Ку', 'Хай', 'Хеллоу']
-        if event.text in vars1:
-            if event.from_user:  # Проверяем откуда направлен наш event
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='Привет, выбери снизу интересующую кнопку!)',
-                )
-        vars2 = ['Информация']
-        if event.text in vars2:
-            if event.from_user:
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text and event.from_user:
+        cur = check(ListOfStudents, event.user_id, size)
+        lock = 1
+        while lock:
+            if cur.position == "lobby":
+                lock -= 1
+                greetings = ['Привет', 'Ку', 'Хай', 'Хеллоу']
+                listOfOptions = ['Викторина', 'Расписание', 'Обратная связь', 'Информация', 'Новости']
+                if event.text in greetings:
+                    out_lobby('Привет, выбери снизу интересующую кнопку!)')
+                elif event.text in listOfOptions:
+                    cur.position = get_position(event.text)
+                    lock += 1
+                else:
+                    out_lobby('Выбери снизу интересующую кнопку!')
+            elif cur.position == "info":
+                lock -= 1
+                university = ['Информация о ВУЗах-партнерах']
+                olimpyc = ['Олимпиады']
+                exitWordsForInfo = ['Назад']
+                genral_info = ['Общая информация']
                 keyboard = VkKeyboard(one_time=True)
-                keyboard.add_button('Общая информация', color=VkKeyboardColor.NEGATIVE)
-                keyboard.add_line()
-                keyboard.add_button('Информация о ВУЗах-партнерах', color=VkKeyboardColor.POSITIVE)
-                keyboard.add_line()
-                keyboard.add_button('Олимпиады', color=VkKeyboardColor.PRIMARY)
-                keyboard.add_button('Назад', color=VkKeyboardColor.PRIMARY)
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='Видимо ты наш новый друг? Рады тебя приветствовать! Что конкретно тебя интересно?'
-                )
-        vars3 = ['Начать']
-        if event.text in vars3:
-            if event.from_user:  # Проверяем откуда направлен наш event
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='Добро пожаловать в группу Энергоклассов ПАО "РусГидро"! Выбери снизу интересующую кнопку!',
-                )
-        vars4 = ['Общая информация']
-        if event.text in vars4:
-            if event.from_user:  # Проверяем откуда направлен наш event
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='РусГидро приглашает школьников девяти регионов России в Энергоклассы и на факультативные занятия по теории решения изобретательских задач (ТРИЗ). В рамках образовательного проекта Энергоклассов учащиеся 9-11 классов изучают профильные предметы гидроэнергетической тематики, знакомятся с компанией и проходят углубленную довузовскую подготовку по физике и математике. Занятия по ТРИЗ ориентированы на школьников 7-8 классов. Все обучение является бесплатным, начало занятий – в октябре.',
-                )
-        vars5 = ['Информация о ВУЗах-партнерах']
-        if event.text in vars5:
-            if event.from_user:  # Проверяем откуда направлен наш event
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='Благодаря Энергоклассам ПАО "РусГидро" у тебя есть шанс поступить в наши ВУЗы-партнеры, среди которых: Санкт-Петербургский политехнический университети им.Петра Великого в Институт Энергетики,Национальный исследовательский университет ИТМО, Московский политехнический университет, Российский государственный аграрный университет МСХА имени К.А. Тимирязева, Московский авиационный институт, Институт гидроэнергетики и возобновляемых источников энергии, АмГУ- кафедра энергетики! Довольно впечатляющий список...',
-                )
-        vars6 = ['Олимпиады']
-        if event.text in vars6:
-            if event.from_user:  # Проверяем откуда направлен наш event
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='У нас ты сможешь принять участие в олимпиадах от ПАО "РусГидро", такие как: Отраслевая олимпиада школьников «Энергия образования» и другие Всероссийские олимпиады! При успешном участии у тебя есть возможность получить дополнительные балы к ЕГЭ при поступлении в наши ВУЗы-партрнеры!',
-                )
-        vars7 = ['Назад']
-        if event.text in vars7 or event.text in vars0:
-            if event.from_user:  # Проверяем откуда направлен наш event
-                keyboard = VkKeyboard(one_time=True)
-                keyboard.add_button('Информация', color=VkKeyboardColor.NEGATIVE)
-                keyboard.add_button('Обратная связь', color=VkKeyboardColor.NEGATIVE)
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='Добро пожаловать в группу Энергоклассов ПАО "РусГидро"! Выбери снизу интересующую кнопку!',
-                )
-        if event.text not in vars0 and event.text not in vars2 and event.text not in vars1 and event.text not in vars3 and event.text not in vars4 and event.text not in vars5 and event.text not in vars6 and event.text not in vars7:
-            if event.from_user:
-                Lsvk.messages.send(
-                    user_id=event.user_id,
-                    random_id=get_random_id(),
-                    keyboard=keyboard.get_keyboard(),
-                    message='Лучше выбери снизу интересующую кнопку)))'
-                )
+                out_info(keyboard)
+                if event.text in university:
+                    Lsvk.messages.send(
+                        user_id=event.user_id,
+                        random_id=get_random_id(),
+                        keyboard=keyboard.get_keyboard(),
+                        message='Благодаря Энергоклассам ПАО "РусГидро" у тебя есть шанс поступить в наши ВУЗы-партнеры, среди которых: Санкт-Петербургский политехнический университети им.Петра Великого в Институт...',
+                    )
+                elif event.text in olimpyc:
+                    Lsvk.messages.send(
+                        user_id=event.user_id,
+                        random_id=get_random_id(),
+                        keyboard=keyboard.get_keyboard(),
+                        message='У нас ты сможешь принять участие в олимпиадах от ПАО "РусГидро", такие как: Отраслевая олимпиада школьников «Энергия образования» и другие Всероссийские олимпиады! При успешном участии...',
+                    )
+                elif event.text in genral_info:
+                    Lsvk.messages.send(
+                        user_id=event.user_id,
+                        random_id=get_random_id(),
+                        keyboard=keyboard.get_keyboard(),
+                        message='РусГидро приглашает школьников девяти регионов России в Энергоклассы и на факультативные занятия по теории решения изобретательских задач (ТРИЗ). В рамках образовательного проекта Энергоклассов учащиеся 9-11 классов...',
+                    )
+                elif event.text in exitWordsForInfo:
+                    lock = exit('lobby', '', cur, lock)
+                else:
+                    Lsvk.messages.send(
+                        user_id=event.user_id,
+                        random_id=get_random_id(),
+                        keyboard=keyboard.get_keyboard(),
+                        message='Выбери снизу интересующую кнопку!',
+                    )
+
+
+            elif cur.position == "schedule":
+                lock -= 1
+                # тело расписания
+                # if event.text in exitWordsForSchedule:
+                #    lock = exit('lobby','',cur, lock) Выход из Расписания закачивать этой функцией
+
+
+            elif cur.position == "feedback":
+                lock -= 1
+                # тело обратной связи
+                feedback(event)
+                #if event.text in exitWordsForFeedback:
+                event.text = " "
+                lock = exit('lobby','',cur, lock) #Выход из Обратной связи закачивать этой функцией
+               # print("lock= ", lock)
+
+            elif cur.position == "news":
+                lock -= 1
+                # тело обратной связи
+                # if event.text in exitWordsForNews:
+                #   lock = exit('lobby','',cur, lock) Выход из Новостей закачивать этой функцией
+
+
+            elif cur.position == "quiz":
+                lock -= 1
+                greetingWordsForQuiz = ['Викторина']
+                startWordsForQuiz = ['Старт', 'Еще раз']
+                exitWordsForQuiz = ['Закончить викторину', 'Выход', 'Назад']
+
+                if event.text in greetingWordsForQuiz:
+                    print_start('Для начала викторины жми \"Старт\"')
+
+                elif event.text in startWordsForQuiz:
+                    cur.game = 1
+                    quest, vars = get_quest(filename, cur, quest, vars)
+                    print_variants(quest, vars)
+
+                elif event.text in exitWordsForQuiz:  # Выход
+                    cur.game = 0
+                    cur.already = 0
+                    cur.score = 0
+                    lock = exit('lobby', 'Удачи!', cur, lock)
+                elif cur.game == 1:
+                    if cur.already < maxsize:
+                        check_answers(event, quest, cur)
+                        quest, vars = get_quest(filename, cur, quest, vars)
+                        print_variants(quest, vars)
+
+                    else:
+                        check_answers(event, quest, cur)
+                        out_result(cur)
+
+                else:
+                    print_start('Попробуй ввести что-то другое')
+
+
